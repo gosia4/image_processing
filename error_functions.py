9bbfa8cc18a57d520537d4032ecd9c91442ace49
Filip Andrzejewski <242207@edu.p.lodz.pl>
2023-10-14 19:27:25 +0200

    error functions semi completed

--- error_functions.py
+++ error_functions.py
@@ -1,8 +1,8 @@
 import support_functions
 import numpy as np
 import math
+import support_functions as sp
 from PIL import Image
-
 
 def mse(image1, image2):
     if image1.size != image2.size:
@@ -12,15 +12,29 @@ def mse(image1, image2):
         img1 = np.array(image1)
         img2 = np.array(image2)
         width, height = image1.size
-        # Kwadrat różnicy elementów i ich suma
         sum_squared_diff = 0
-        squared_diff = np.zeros((width, height))
         for i in range(width):
             for j in range(height):
-                squared_diff[i, j] = (img1[i, j].astype(float) - img2[i, j].astype(
-                    float)) ** 2  # konwertuje pixele na float
-                sum_squared_diff += squared_diff[i, j]
+                sum_squared_diff += (img1[i, j].astype(int) - img2[i, j].astype(int)) ** 2
 
+        # Calculate the mean squared error
+        mse_value = sum_squared_diff / (width * height)
+        return mse_value
+
+def mse_no_numpy(image1, image2):
+    if image1.size != image2.size:
+        print("Can not compare images of different dimensions")
+        return
+    else:
+
+        width, height = image1.size
+        color_channels = sp.analyse_color_channels(image1)[1]
+        sum_squared_diff = 0
+        for x in range(width):
+            for y in range(height):
+                old_value = sp.int_or_tuple_to_array(image1.getpixel((x, y)))
+                new_value = sp.int_or_tuple_to_array(image2.getpixel((x, y)))
+                sum_squared_diff += (old_value - new_value) ** 2
         # Calculate the mean squared error
         mse_value = sum_squared_diff / (width * height)
         return mse_value
@@ -28,33 +42,23 @@ def mse(image1, image2):
 
 def pmse(image1, image2):
     mse_value = mse(image1, image2)
-    img1 = np.array(image1)
-    width, height = image1.size
-
-    max_squared_value = 0
-    for i in range(width):
-        for j in range(height):
-            if img1[i][j] > max_squared_value:
-                max_squared_value = img1[i][j]
-
-    pmse_value = mse_value / (max_squared_value ** 2)
+    pmse_value = mse_value / np.array([65025, 65025, 65025])
     return pmse_value
-
 
 def snr(image1, image2):
     img1 = np.array(image1)
-    img2 = np.array(image2)
     width, height = image1.size
-    # Calculate the sum of squared differences
-    sum_squared_diff = 0
+
+    color_channels_1 = sp.analyse_color_channels(image1)[1]
+    color_channels_2 = sp.analyse_color_channels(image2)[1]
+
+    mse_value = mse(image1, image2)
     sum_squared_signal = 0
+    snr_value = np.array([0, 0, 0])
 
     for i in range(width):
         for j in range(height):
-            squared_diff = (img1[i, j].astype(float) - img2[i, j].astype(float)) ** 2
-            sum_squared_diff += squared_diff
-            sum_squared_signal += (img1[i, j].astype(float) ** 2)
-
+            sum_squared_signal += img1[i, j].astype(int) ** 2
     # wersja 1
     # # Kwadrat różnicy elementów
     # squared_diff = (img1.astype(float) - img2.astype(float)) ** 2
@@ -65,12 +69,42 @@ def snr(image1, image2):
     #     for j in range(height):
     #         sum_squared_diff += squared_diff[i, j]
 
-    if sum_squared_diff == 0:
-        snr_value = float('inf')  # Set SNR to infinity for zero noise
+    if color_channels_1 * color_channels_2 == 1:
+        if mse_value != 0:
+            snr_value = 10 * np.log10(sum_squared_signal / mse_value)
+
+    elif np.any(mse_value == 0):
+        for i in range(3):
+            if mse_value[i] != 0:
+                snr_value[i] = float('inf')
+            elif color_channels_1 == 1:
+                snr_value[i] = 10 * np.log10(sum_squared_signal / mse_value[i])
+            else:
+                snr_value[i] = 10 * np.log10(sum_squared_signal[i] / mse_value[i])
+
     else:
-        snr_value = 10 * np.log10(sum_squared_signal / sum_squared_diff)
+        snr_value = 10 * np.log10(sum_squared_signal / mse_value)
     return snr_value
 
+
+def snr2(image1, image2):
+    width, height = image1.size
+    mse_value = mse(image1, image2)
+    sum_squared = 0
+    sum_squared_signal = np.array([0, 0, 0])
+    for x in range(width):
+        for y in range(height):
+            sum_squared += sp.int_or_tuple_to_array(image1.getpixel((x, y)))
+
+    if np.any(mse_value == 0):
+        for i in range(3):
+            sum_squared_signal[i] = sum_squared[i] / mse_value[i]
+
+    else:
+        sum_squared_signal = sum_squared / mse_value
+
+    snr_value = 10 * np.log10(sum_squared_signal)
+    return snr_value
 
 def psnr(image1, image2):
     img1 = np.array(image1)
