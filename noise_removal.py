607cdee88e7f9fb931c4a06cefbe3e946b27656d
242218 Małgorzata Komorowska <242218@edu.p.lodz.pl>
2023-10-16 21:45:05 +0200

    geometric filter, works probably

--- noise_removal.py
+++ noise_removal.py
@@ -1,15 +1,18 @@
 import numpy as np
 from PIL import Image
+import support_functions as sp
 
 
 def remove_noise_median(image, kernel_size):
     width, height = image.size
-    # print(image.mode)
-    filtered_image = Image.new("L", (width, height))
+    result_image, color_channels = sp.analyse_color_channels(image)
+
+    if color_channels == 0:
+        print("this program does not support this color model.")
+        return
 
     for x in range(width):
         for y in range(height):
-
             sample_arr = []
 
             for i in range(-kernel_size // 2 + 1, kernel_size // 2 + 1):
@@ -20,44 +23,64 @@ def remove_noise_median(image, kernel_size):
 
                     if (target_x >= 0 and target_x < width - 1):
                         if (target_y >= 0 and target_y < height - 1):
+
                             sample_arr.append(image.getpixel((target_x, target_y)))
+            median_pixel = []
+            for c in range(color_channels):
+                channel_arr = []
+                for h in range(len(sample_arr)):
+                    channel_arr.append(sp.int_or_tuple_to_array(sample_arr[h])[c])
 
-            sample_arr.sort()
-            if kernel_size % 2:
-                median = sample_arr[len(sample_arr) // 2]
+                channel_arr.sort()
 
-            else:
-                median = int((sample_arr[len(sample_arr) // 2] + sample_arr[len(sample_arr) // 2 + 1]) / 2)
+                middle_number = len(channel_arr) // 2
+                if kernel_size % 2:
+                    median_pixel.append(channel_arr[middle_number])
 
-            filtered_image.putpixel((x, y), median)
+                else:
+                    median_pixel.append((channel_arr[middle_number] + channel_arr[middle_number + 1]) // 2)
 
-    filtered_image.save("new_image.bmp")
-    filtered_image.show()
-    return filtered_image
+            result_image.putpixel((x, y), tuple(median_pixel))
+
+    result_image.save("new_image.bmp")
+    result_image.show()
 
 
 def geometric_mean(image, kernel_size):
-    width, height = image.size
-    print(image.mode)
-    filtered_image = Image.new("L", (width, height))
+    if kernel_size < 3:
+        print("Kernel size must be at least 3")
+        return
+    else:
+        width, height = image.size
+        result_image, color_channels = sp.analyse_color_channels(image)
 
-    for x in range(width):
-        for y in range(height):
+        if color_channels == 0:
+            print("this program does not support this color model.")
+            return
+        for x in range(width):
+            for y in range(height):
+                sample_arr = []
 
-            sample_product = 1  # Inicjowanie zmiennej dla obliczenia średniej geometrycznej
+                for i in range(-kernel_size // 2 + 1, kernel_size // 2 + 1):
+                    for j in range(-kernel_size // 2 + 1, kernel_size // 2 + 1):
+                        target_x = x + i
+                        target_y = y + j
 
-            for i in range(-kernel_size // 2 + 1, kernel_size // 2 + 1):
-                for j in range(-kernel_size // 2 + 1, kernel_size // 2 + 1):
+                        if 0 <= target_x < width - 1:
+                            if 0 <= target_y < height - 1:
+                                sample_arr.append(image.getpixel((target_x, target_y)))
+                mean_pix = []
+                for c in range(color_channels):
+                    channel_arr = []
+                    for h in range(len(sample_arr)):
+                        channel_arr.append(sp.int_or_tuple_to_array(sample_arr[h])[c])
+                    mean_value = 1.0
+                    for val in channel_arr:
+                        mean_value *= val
+                    mean_value = mean_value ** (1 / len(channel_arr))
+                    mean_pix.append(int(mean_value))
 
-                    target_x = x + i
-                    target_y = y + j
+                result_image.putpixel((x, y), tuple(mean_pix))
 
-                    if 0 <= target_x < width - 1:
-                        if target_y >= 0 and target_y < height - 1:
-                            # Mnożenie wartości piksela do obliczenia średniej geometrycznej
-                            sample_product *= image.getpixel((target_x, target_y))
+        result_image.show()
 
-            # Obliczenie średniej geometrycznej
-            geometric_mean = sample_product ** (1.0 / (kernel_size * kernel_size))
-            filtered_image.putpixel((x, y), int(geometric_mean))
-    filtered_image.show()
