86f915f9d6855abebe18e72cf82377a7fd7e1025
Filip Andrzejewski <242207@edu.p.lodz.pl>
2023-10-22 17:40:19 +0200

    help function done + raport update

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import sys
+from PIL import Image
 
 import error_functions
 import geometric_operations
 import basic_operations
 import noise_removal
-from PIL import Image
-import numpy as np
 
-geometric_operations.resize(Image.open("lenac.bmp"), 1, "new_image.bmp")
+
 
 def show_help():
-    print("------------Basic operations:----------------\n"
+    print("---------------Basic operations:-----------------\n"
           "\n"
           "--brightness: modifies brightness of the image by value\n"
           "\tuse case: --brightness [image_path] [value] [output_path]\n"
@@ -27,7 +26,7 @@ def show_help():
           "--negative: creates the negative of the image\n"
           "\tuse case: --negative [image_path] [output_path]\n"
           "\n"
-          "------------Geometric operations:----------------\n"
+          "-------------Geometric operations:----------------\n"
           "\n"
           "--hflip: flips the image horizontally\n"
           "\tuse case: --hflip [image_path] [output_path]\n"
@@ -48,7 +47,7 @@ def show_help():
           "\t\t[value] describes the proportions between output image size and original size.\n"
           "\t\tThe output image will be [value] times larger than the original image.\n"
           "\n"
-          "------------Noise Filters:----------------\n"
+          "------------------Noise Filters:------------------\n"
           "\n" 
           "--median: reduces the noise of the image using median filter\n"
           "\tuse case: --median [image_path] [kernel_size] [output_path]\n"
@@ -60,13 +59,33 @@ def show_help():
           "\t\t[kernel_size] describes the dimensions of samples that filter will be using (minimum 3).\n"
           "\t\tFor best performance provide the [kernel size] == 3\n"
           "\n"
-          "------------Error Calculation:----------------\n"
+          "---------------Error Calculation:-----------------\n"
           "\n"
-          "--mse: returns Mean square error value "
-          "\n\n--pmse: Peak mean square error"
-          "\n\n--snr: Signal to noise ratio"
-          "\n\n--psnr: Peak signal to noise ratio"
-          "\n\n--md: Maximum difference")
+          "--mse: returns Mean square error value for pair of images\n"
+          "\tuse case: --mse [first_image_path] [second_image_path]\n"
+          "\t\tMse will be calculated regarding individual pixels of those 2 images.\n"
+          "\t\tFirst image is treated as original image and second one as it's modification.\n"
+          "\n"
+          "--pmse: returns Peak mean square error value for pair of images\n"
+          "\tuse case: --pmse [first_image_path] [second_image_path]\n"
+          "\t\tPmse will be calculated regarding individual pixels of those 2 images.\n"
+          "\t\tFirst image is treated as original image and second one as it's modification.\n"
+          "\n"
+          "--snr: returns Signal to noise ratio value for pair of images\n"
+          "\tuse case: --snr [first_image_path] [second_image_path]\n"
+          "\t\tSnr will be calculated regarding individual pixels of those 2 images.\n"
+          "\t\tFirst image is treated as original image and second one as it's modification.\n"
+          "\n"
+          "--psnr: returns Peak signal to noise ratio value for pair of images\n"
+          "\tuse case: --psnr [first_image_path] [second_image_path]\n"
+          "\t\tPsnr will be calculated regarding individual pixels of those 2 images.\n"
+          "\t\tFirst image is treated as original image and second one as it's modification.\n"
+          "\n"
+          "--md: returns Maximum differencevalue for pair of images\n"
+          "\tuse case: --md [first_image_path] [second_image_path]\n"
+          "\t\tMd will be calculated regarding individual pixels of those 2 images.\n"
+          "\t\tFirst image is treated as original image and second one as it's modification.\n"
+          )
 
 show_help()
 if len(sys.argv) < 2:
